079539deb899ae9048c3125303c2cb88f2144a62
SV-wang-xiaochen <xiaochen.wang@intalight.com>
2023-10-23 23:31:25 +0800

    regenerate obj file by open3d

---
+++ saveObjByOpen3d.py
@@ -0,0 +1,20 @@
+# The .obj file in our dataset stores the mesh by the order of vertices. We can check the index of vertices in Meshlab.
+# o3d.io.read_triangle_mesh can load .obj file, however it loads by the order of faces rather than vertices.
+# Therefore, the index of vertices seen from Meshlab is not correct anymore.
+
+# Solution: Load the .obj file by o3d.io.read_triangle_mesh and then save it by o3d.io.write_triangle_mesh
+
+import open3d as o3d
+import glob
+
+def main():
+    path = r'C:\Users\xiaochen.wang\Projects\Dataset\FLORENCE'
+    list = glob.glob(f'{path}/**/*.obj', recursive=True)
+
+    for item in list:
+        mesh = o3d.io.read_triangle_mesh(item)
+
+        o3d.io.write_triangle_mesh(item, mesh)
+
+if __name__ == "__main__":
+    main()
