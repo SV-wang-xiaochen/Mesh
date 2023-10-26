5d064697f93795aa8a0d06b8e866fee6764fd012
SV-wang-xiaochen <xiaochen.wang@intalight.com>
2023-10-26 16:21:18 +0800

    Only keep line segments within the lens

--- plane_mesh_intersection.py
+++ plane_mesh_intersection.py
@@ -5,12 +5,32 @@ import copy
 
 Z_LENS = 12 # Z distance from Lens plane to LeftEyeFront point. Range: 3-30 mm
 
+SHOW_FULL_MESH = True # Show full mesh or ONLY "mesh of interest" which is used to find the intersection with lens plane
+
 LeftEyeRear = 4463
 Head1 = 1726
 Head2 = 1335
 Head3 = 1203
 MOUTH_ABOVE = 825
 BROW_ABOVE = 2295
+
+def line_segment_with_circle(line_segment, circle_origin, circle_radius):
+    """ Check if a line segment is within a circle. Here the line segment must be within the plane where the circle is within.
+        Here we define that if any point of the line segment is within the circle, then True is returned.
+    :param line_segment: line segment [start_point, end_point]
+    :param circle_origin: origin of circle
+    :param circle_radius: radius of circle
+    :return isWithinCircle: Boolean
+    """
+
+    distance1 = np.linalg.norm(line_segment[0] - circle_origin)
+    distance2 = np.linalg.norm(line_segment[1] - circle_origin)
+
+    # Check if the point is within the circle
+    if distance1 < circle_radius or distance2 < circle_radius:
+        return True
+    else:
+        return False
 
 path = r'C:\Users\xiaochen.wang\Projects\Dataset\FLORENCE'
 obj_list = glob.glob(f'{path}/**/*.obj', recursive=True)
@@ -46,7 +66,45 @@ face_mask = vertices_mask[mesh.faces].all(axis=1)
 mesh.update_faces(face_mask)
 
 scene = trimesh.Scene()
-scene.add_geometry(mesh_original)
+if SHOW_FULL_MESH:
+    scene.add_geometry(mesh_original)
+else:
+    scene.add_geometry(mesh)
+
+####################### draw the max lens (a circle with diameter 80mm) #######################
+
+# Define the parameters
+origin_lens = np.array([0, 0, Z_LENS/1000])
+radius = 0.04
+num_points = 100  # Number of points to create the circle
+
+# Create an array of angles to parametrically define the circle
+theta = np.linspace(0, 2 * np.pi, num_points)
+
+# Parametric equations to generate points on the circle
+x = radius * np.cos(theta) + origin_lens[0]
+y = radius * np.sin(theta) + origin_lens[1]
+z = np.full(num_points, origin_lens[2])  # Constant z-coordinate
+
+# Create an array to store the line segments
+circle_segments = []
+
+# Connect the points to create line segments
+for i in range(num_points - 1):
+    circle_segments.append([[x[i], y[i], z[i]], [x[i + 1], y[i + 1], z[i + 1]]])
+
+# Connect the last point to the first point to close the circle
+circle_segments.append([[x[-1], y[-1], z[-1]], [x[0], y[0], z[0]]])
+
+# Convert the line segments to a NumPy array
+circle_segments = np.array(circle_segments)
+
+circle = trimesh.load_path(circle_segments)
+scene.add_geometry(circle)
+
+# plot the LeftEyeFront point
+origin = trimesh.points.PointCloud(vertices=[[0, 0, 0]], colors=(255, 0, 0))
+scene.add_geometry(origin)
 
 ####################### find intersection between lens plane and mesh #######################
 
@@ -55,46 +113,15 @@ point_on_plane = [0, 0, Z_LENS/1000]
 plane_normal = [0, 0, 1]
 
 # Find the intersection between the mesh and the plane
-lines = trimesh.intersections.mesh_plane(mesh, plane_normal, point_on_plane)
-print(len(lines))
+intersection_segments = trimesh.intersections.mesh_plane(mesh, plane_normal, point_on_plane)
 
-intersection = trimesh.load_path(lines)
-scene.add_geometry(intersection)
+#######################  Only keep line segments within the lens #######################
+intersection_segments_within_circle = []
+for segment in intersection_segments:
+    if line_segment_with_circle(segment, origin_lens, radius):
+        intersection_segments_within_circle.append(segment)
 
-####################### draw the max lens (a circle with diameter 80mm) #######################
-
-# Define the parameters
-origin = np.array([0, 0, Z_LENS/1000])
-radius = 0.04
-num_points = 100  # Number of points to create the circle
-
-# Create an array of angles to parametrically define the circle
-theta = np.linspace(0, 2 * np.pi, num_points)
-
-# Parametric equations to generate points on the circle
-x = radius * np.cos(theta) + origin[0]
-y = radius * np.sin(theta) + origin[1]
-z = np.full(num_points, origin[2])  # Constant z-coordinate
-
-# Create an array to store the line segments
-line_segments = []
-
-# Connect the points to create line segments
-for i in range(num_points - 1):
-    line_segments.append([[x[i], y[i], z[i]], [x[i + 1], y[i + 1], z[i + 1]]])
-
-# Connect the last point to the first point to close the circle
-line_segments.append([[x[-1], y[-1], z[-1]], [x[0], y[0], z[0]]])
-
-# Convert the line segments to a NumPy array
-line_segments = np.array(line_segments)
-
-circle = trimesh.load_path(line_segments)
-scene.add_geometry(circle)
-
-# plot the LeftEyeFront point
-origin = trimesh.points.PointCloud(vertices=[[0, 0, 0]], colors=(255, 0, 0))
-scene.add_geometry(origin)
+intersection_segments_within_circle = trimesh.load_path(np.array(intersection_segments_within_circle))
+scene.add_geometry(intersection_segments_within_circle)
 
 scene.show(smooth=False)
-
