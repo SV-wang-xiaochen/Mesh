af8e84dc5526cbe4bb1e784aaf520ee8aee89ef3
SV-wang-xiaochen <xiaochen.wang@intalight.com>
2023-10-31 18:01:22 +0800

    loop over all heads and lens scale to do stats

--- mesh_mesh_intersection_stats.py
+++ mesh_mesh_intersection_stats.py
@@ -16,21 +16,7 @@ ref_vertex_index = 0
 eye_ball_centroid = [0, 0, -1.30439425e-02] # Pre-calculated by averaging 53 EyeBallCentroid
 lens_half_height_after_cut = 22
 lens_init_centroid_z = 12
-lens_scale = 1 # When scale is 1, the diameter of the lens is around 54 mm
-
-# # Define the semi-axes lengths of the ellipsoid lens
-# lens_scale = 10
-# lens_semi_x = 3 # eye left-right direction
-# lens_semi_y = 3 # eye up-down direction
-# lens_semi_z = 0.1 # eye front-rear direction
-#
-# # Define the rotation angles in degrees
-# angle_x = 0  # Eye up-down Rotation around x-axis, + means down
-# angle_y = 0  # Eye left-right Rotation around y-axis, + means left
-#
-# # Define lens centroid
-# lens_init_centroid_x = 0
-# lens_init_centroid_y = 0
+lens_scale_list = [0.5, 0.6, 0.7, 0.8, 0.9, 1] # When scale is 1, the diameter of the lens is around 54 mm
 
 def rotation_matrix_from_vectors(vec1, vec2):
     """ Find the rotation matrix that aligns vec1 to vec2
@@ -74,8 +60,8 @@ def angle_between_two_vectors(vec1, vec2):
 
 
 def ellipsoid(a, b, c):
-    u = np.linspace(0, 2 * np.pi, 100)
-    v = np.linspace(0, np.pi, 50)
+    u = np.linspace(0, 2 * np.pi, 75)
+    v = np.linspace(0, np.pi, 75)
     x = a * np.outer(np.cos(u), np.sin(v))
     y = b * np.outer(np.sin(u), np.sin(v))
     z = c * np.outer(np.ones(np.size(u)), np.cos(v))
@@ -104,103 +90,110 @@ def o3dTriangleMesh2PointCloud(mesh):
     return point_cloud
 
 # #######################  Load lens mesh #######################
+for lens_scale in lens_scale_list:
 
-lens_path = r'C:\Users\xiaochen.wang\Projects\Dataset\lens_z12.obj'
-lens_mesh_original = trimesh.load_mesh(lens_path)
-lens_mesh_original.visual.face_colors = [64, 64, 64, 100]
+    lens_path = r'C:\Users\xiaochen.wang\Projects\Dataset\lens_z12.obj'
+    lens_mesh_original = trimesh.load_mesh(lens_path)
+    lens_mesh_original.visual.face_colors = [64, 64, 64, 100]
 
-# cut the lens at the top and bottom
-lens_mesh = trimesh.intersections.slice_mesh_plane(trimesh.intersections.slice_mesh_plane(lens_mesh_original,
-[0,-1,0], [0,lens_half_height_after_cut/1000,0]),[0,1,0], [0,-lens_half_height_after_cut/1000,0])
+    # cut the lens at the top and bottom
+    lens_mesh = trimesh.intersections.slice_mesh_plane(trimesh.intersections.slice_mesh_plane(lens_mesh_original,
+    [0,-1,0], [0,lens_half_height_after_cut/1000,0]),[0,1,0], [0,-lens_half_height_after_cut/1000,0])
 
-# scale the lens
-lens_mesh.apply_translation([0, 0, -lens_init_centroid_z/1000])
-lens_mesh.apply_scale(lens_scale)
-lens_mesh.apply_translation([0, 0, lens_init_centroid_z/1000])
+    # scale the lens
+    lens_mesh.apply_translation([0, 0, -lens_init_centroid_z/1000])
+    lens_mesh.apply_scale(lens_scale)
+    lens_mesh.apply_translation([0, 0, lens_init_centroid_z/1000])
 
-# Translate the coordinates so that the centroid of eye ball becomes the origin
-lens_mesh.apply_translation([-eye_ball_centroid[0], -eye_ball_centroid[1], -eye_ball_centroid[2]])
+    # Translate the coordinates so that the centroid of eye ball becomes the origin
+    lens_mesh.apply_translation([-eye_ball_centroid[0], -eye_ball_centroid[1], -eye_ball_centroid[2]])
 
-path = r'C:\Users\xiaochen.wang\Projects\Dataset\FLORENCE'
-obj_list = glob.glob(f'{path}/**/*.obj', recursive=True)
 
-head_hits = np.zeros(5000) # this value only works when len(vertices_valid) <= 5000
+    path = r'C:\Users\xiaochen.wang\Projects\Dataset\FLORENCE'
+    obj_list = glob.glob(f'{path}/**/*.obj', recursive=True)
 
-for mesh_nr in range(0, 2):
+    head_hits = np.zeros(5000) # this value only works when len(vertices_valid) <= 5000
 
-    mesh_original = trimesh.load_mesh(obj_list[mesh_nr])
+    for mesh_nr in range(0, 2):
 
-    mesh_original.visual.face_colors = [64, 64, 64, 100]
+        mesh_original = trimesh.load_mesh(obj_list[mesh_nr])
 
-    # Create 3d sphere, which is the region where centroid of the lens could be located
-    sphere_radius = lens_init_centroid_z/1000-eye_ball_centroid[2]
+        mesh_original.visual.face_colors = [64, 64, 64, 100]
 
-    # Generate sphere points
-    x, y, z = ellipsoid(sphere_radius, sphere_radius, sphere_radius)
+        # Create 3d sphere, which is the region where centroid of the lens could be located
+        sphere_radius = lens_init_centroid_z/1000-eye_ball_centroid[2]
 
-    # Create a trimesh object from vertices and faces
-    vertices = np.stack((x.flatten(), y.flatten(), z.flatten()), axis=-1)
+        # Generate sphere points
+        x, y, z = ellipsoid(sphere_radius, sphere_radius, sphere_radius)
 
-    # Get the vertices which are within the 15 degree region
-    if mesh_nr == 0:
-        vertices_valid = []
-    valid_point_index = 0
-    print(f"all vertices: {len(vertices)} ")
-    for vertices_index, sphere_point in enumerate(vertices):
-        # put only <= 15 degree here after debug
-        if (angle_between_two_vectors([0,0,1], sphere_point) <= 15
-                and angle_between_two_vectors([0,0,1], sphere_point) >= 0
-                and sphere_point[0]>=0 and sphere_point[2]>=0 and sphere_point[1]<=0):
-            print(f"vertices {vertices_index} is valid")
-            if mesh_nr == 0:
-                vertices_valid.append(sphere_point)
+        # Create a trimesh object from vertices and faces
+        vertices = np.stack((x.flatten(), y.flatten(), z.flatten()), axis=-1)
 
-            # Rotate lens
-            ref_vertex = sphere_point
-            print(f"Before rotation, the angle in degree between ref vertex and lens centroid: {angle_between_two_vectors([0,0,1], ref_vertex)}\n")
-            R = rotation_matrix_from_vectors([0,0,1], ref_vertex)
-            Rotation = np.eye(4)
-            Rotation[:3, :3] = R
+        # Get the vertices which are within the 15 degree region
+        if mesh_nr == 0:
+            vertices_valid = []
+        valid_point_index = 0
+        print(f"all vertices: {len(vertices)} ")
+        for vertices_index, sphere_point in enumerate(vertices):
+            # put only <= 15 degree here after debug
+            if (angle_between_two_vectors([0,0,1], sphere_point) <= 15
+                    and angle_between_two_vectors([0,0,1], sphere_point) >= 0
+                    and sphere_point[0]>0 and sphere_point[2]>0 and sphere_point[1]<0):
+                print(f"lens scale {lens_scale}, vertices {vertices_index} is valid")
+                if mesh_nr == 0:
+                    vertices_valid.append(sphere_point)
 
-            lens_mesh.apply_transform(Rotation)
+                # Rotate lens
+                ref_vertex = sphere_point
+                print(ref_vertex)
+                print(f"Before rotation, the angle in degree between ref vertex and lens centroid: {angle_between_two_vectors([0,0,1], ref_vertex)}")
+                R = rotation_matrix_from_vectors([0,0,1], ref_vertex)
+                Rotation = np.eye(4)
+                Rotation[:3, :3] = R
 
-            # check intersection between lens_mesh and mesh_original
-            intersections = trimesh.boolean.intersection([lens_mesh, mesh_original], engine='blender')
+                lens_mesh.apply_transform(Rotation)
 
-            if hasattr(intersections, 'vertices'):
-                print(f"Head {mesh_nr}, Vertices {vertices_index}/{len(vertices)}")
-                print("intersected")
-                head_hits[valid_point_index] += 1
-            else:
-                print(f"Head {mesh_nr}, Vertices {vertices_index}/{len(vertices)}")
-                print("NOT intersected")
-            valid_point_index += 1
-        else:
-            print(f"vertices {vertices_index} is NOT valid")
-    if mesh_nr == 0:
-        vertices_valid = np.array(vertices_valid)
-        print(vertices_valid.shape)
+                lens_mesh.apply_translation([eye_ball_centroid[0], eye_ball_centroid[1], eye_ball_centroid[2]])
 
-print("head hit results:")
-print(head_hits[:len(vertices_valid)])
+                # check intersection between lens_mesh and mesh_original
+                intersections = trimesh.boolean.intersection([lens_mesh, mesh_original], engine='blender')
+                if hasattr(intersections, 'vertices'):
+                    print(f"Head {mesh_nr}, Vertices {vertices_index}/{len(vertices)}")
+                    print("intersected\n")
+                    head_hits[valid_point_index] += 1
+                else:
+                    print(f"Head {mesh_nr}, Vertices {vertices_index}/{len(vertices)}")
+                    print("NOT intersected\n")
+                valid_point_index += 1
+        if mesh_nr == 0:
+            vertices_valid = np.array(vertices_valid)
+            print(vertices_valid.shape)
 
-# Plot 3d points from vertices_valid. Color with the value of head hit.
-x = vertices_valid[:, 0]*1000
-y = vertices_valid[:, 1]*1000
-z = vertices_valid[:, 2]*1000
+    print("head hit results:")
+    print(head_hits[:len(vertices_valid)])
 
-# Create a 3D plot
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+    # Plot 3d points from vertices_valid. Color with the value of head hit.
+    x = vertices_valid[:, 0]*1000
+    y = vertices_valid[:, 1]*1000
+    z = vertices_valid[:, 2]*1000
 
-# Plot the points
-colors = x # placeholder, later replace with the value of head hit.
-ax.scatter(x, y, z, c=head_hits[:len(vertices_valid)], cmap='viridis', marker='o')
+    # Create a 3D plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
 
-# Set labels
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
+    # Plot the points
+    colors = x # placeholder, later replace with the value of head hit.
+    ax.scatter(x, y, z, c=head_hits[:len(vertices_valid)], cmap='inferno', marker='o')
 
-# Show the plot
-plt.show()
+    # Set labels
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    # Show the plot
+    plt.savefig(f"{lens_scale}_3d.png")
+
+    result_to_save =[x, y, z, head_hits[:len(vertices_valid)]]
+
+    np.save(f"{lens_scale}", result_to_save)
+
