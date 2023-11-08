218c7d50831a86aa8b1a06622cb43000d4cee627
SV-wang-xiaochen <xiaochen.wang@intalight.com>
2023-11-08 09:26:54 +0800

    show the reference overlapped head

--- visualize_results_voxel.py
+++ visualize_results_voxel.py
@@ -1,7 +1,7 @@
 import trimesh
 import numpy as np
 
-PITCH = 0.005
+PITCH = 0.0025
 num_of_heads = np.load(f"voxel_results/num_of_heads_{PITCH}.npy")
 voxel_center_min = np.load(f"voxel_results/voxel_center_min_{PITCH}.npy")
 voxel_center_max = np.load(f"voxel_results/voxel_center_max_{PITCH}.npy")
@@ -14,5 +14,10 @@ scene = trimesh.Scene()
 
 V = trimesh.PointCloud(vertices=voxel_list_remove_zero, colors=colors_list)
 
-scene.add_geometry(V)
+# show the voxels whose head hits >=3, this is the reference head that the lens must not intersect
+valid_voxel = [voxel_list_remove_zero[i] for i in range(len(accumulation_remove_zero)) if accumulation_remove_zero[i] >= 3]
+valid_hit = [colors_list[i] for i in range(len(accumulation_remove_zero)) if accumulation_remove_zero[i] >= 3]
+V_valid = trimesh.PointCloud(vertices=valid_voxel, colors=valid_hit)
+
+scene.add_geometry(V_valid)
 scene.show(smooth=False, flags={'wireframe': True})
