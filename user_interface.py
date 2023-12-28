98fe29b1b3104e8622f061e518aeac923e7263e8
SV-wang-xiaochen <xiaochen.wang@intalight.com>
2023-12-28 09:40:56 +0800

    update input fields to mesh project

--- user_interface.py
+++ user_interface.py
@@ -3,7 +3,7 @@ from tkinter import messagebox
 
 INTERACTIVE_INPUT = True  # Set this to False if you want to use default values
 
-def process_input(entries, root, next_window, run_mesh, mode1, mode2):
+def process_input(entries, root, next_window, run_mesh):
     try:
         # Extract values from the entries
         args = [entry.get() for entry in entries.values()]
@@ -16,7 +16,9 @@ def process_input(entries, root, next_window, run_mesh, mode1, mode2):
 
         if run_mesh:
             # Additional code to run after processing the arguments
-            print(f"工作模式：{mode1},{mode2}")
+            print(f"工作模式：")
+            for i, arg in enumerate(args, start=1):
+                print(f"Argument {i}: {arg}")
             print("Running additional code...")
 
         # Destroy the root window (current window)
@@ -36,7 +38,7 @@ def open_first_window(*args):
     entries = create_first_window_input_fields(first_window)
 
     # Create a button that calls the process_input function when clicked
-    button_process = tk.Button(first_window, text="Process Input", command=lambda: process_input(entries, first_window, open_second_window, False, 0, 0))
+    button_process = tk.Button(first_window, text="Process Input", command=lambda: process_input(entries, first_window, open_second_window, False))
     button_process.grid(row=2, column=0, columnspan=2, pady=10)
 
 def open_second_window(*args):
@@ -47,8 +49,8 @@ def open_second_window(*args):
     entries = create_second_window_input_fields(second_window, *args)
 
     # Create a button that calls the process_input function when clicked
-    button_process = tk.Button(second_window, text="Process Input", command=lambda: process_input(entries, second_window, lambda *args: open_first_window(*args), True, *args))
-    button_process.grid(row=2, column=0, columnspan=2, pady=10)
+    button_process = tk.Button(second_window, text="Process Input", command=lambda: process_input(entries, second_window, lambda *args: open_first_window(*args), True))
+    button_process.grid(row=15, column=0, columnspan=2, pady=10)
 
 def create_first_window_input_fields(root):
     # Create a dictionary to store the entry widgets
@@ -76,20 +78,36 @@ def create_second_window_input_fields(root, *args):
     # Create a dictionary to store the entry widgets
     entries = {}
 
+    arg_label_text = ['工作距离(周边)[0,45]mm:']
 
     # Use arg1 to determine the number and labels for entry widgets
+    if args[1] == '2':
+        arg_label_text.append('面板模型文件名(无后缀):')
+    if args[1] == '1':
+        arg_label_text.append('目镜外框直径[20,80]mm:')
+        arg_label_text.append('通光孔径[20,80]mm:')
+        arg_label_text.append('单张范围（眼外角）[70,140]度:')
+
     if args[0] == '1':
-        arg_label_text = ['面板模型文件名(无后缀):', '工作距离(周边)[0,45]mm:']
-    elif args[1] == '2':
-        arg_label_text = ['11面板模型文件名(无后缀):', '11工作距离(周边)[0,45]mm:']
-    else:
-        arg_label_text = ['22面板模型文件名(无后缀):', '22工作距离(周边)[0,45]mm:']
+        arg_label_text.append('机器俯仰角[-90,90]度(+仰,-俯):')
+        arg_label_text.append('机器内外旋角[-90,90]度(+内旋,-外旋):')
+        arg_label_text.append('眼睛俯仰角[-90,90]度(+俯,-仰):')
+        arg_label_text.append('眼睛内外旋角[-90,90]度(+外旋,-内旋):')
+    if args[0] == '2':
+        arg_label_text.append('机器俯仰角 最小值(+仰,-俯):')
+        arg_label_text.append('机器俯仰角 区间长度:')
+        arg_label_text.append('机器俯仰角 步进角度:')
+        arg_label_text.append('机器内外旋角 最小值(+内旋,-外旋):')
+        arg_label_text.append('机器内外旋角 区间长度:')
+        arg_label_text.append('机器内外旋角 步进角度:')
+        arg_label_text.append('俯仰眼位 夹角[-30,+30]度:')
+        arg_label_text.append('鼻颞眼位 夹角[-30,+30]度:')
 
     # Create entry widgets for each argument using a for loop
     for i, arg_text in enumerate(arg_label_text):
         arg_label = tk.Label(root, text=arg_text)
         arg_entry = tk.Entry(root)
-        entries['arg'+str(i)] = arg_entry
+        entries[arg_text] = arg_entry
 
         # Position the widgets using the grid layout
         arg_label.grid(row=i, column=0, padx=5, pady=5, sticky="e")
